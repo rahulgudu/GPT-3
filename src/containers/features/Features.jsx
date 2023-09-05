e7e87275c3d5ca1313069598ad922d2826557eff
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-09-05 10:57:55 +0530

    features section is completed which is a part of case studies in navbar

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
-import "./Features";
+import React from "react";
+import { Feature } from "../../components";
+import "./feature.css";
 const Features = () => {
+  const featuresData = [
+    {
+      title: "Improving and distrusts instantly",
+      text: "From they jfb fjfbnvnvfbv vfjvbfjvbf v fbjkfewf dh  vjfbejhvfkjv bvh vjvbhj e vfebvhfv ",
+    },
+    {
+      title: "Become the tended active",
+      text: "From they jfb fjfbnvnvfbv vfjvbfjvbf v fbjkfewf dh  vjfbejhvfkjv bvh vjvbhj e vfebvhfv ",
+    },
+    {
+      title: "Message or am nothing",
+      text: "From they jfb fjfbnvnvfbv vfjvbfjvbf v fbjkfewf dh  vjfbejhvfkjv bvh vjvbhj e vfebvhfv ",
+    },
+    {
+      title: "Really boy law county",
+      text: "From they jfb fjfbnvnvfbv vfjvbfjvbf v fbjkfewf dh  vjfbejhvfkjv bvh vjvbhj e vfebvhfv ",
+    },
+  ];
   return (
-    <div>
-      Features
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">
+          The Future is Now and Need To Realize It. Step into Future Today &
+          Make it Happen
+        </h1>
+        <p>Request Early Access to get Started</p>
+      </div>
+      <div className="gpt3__features-container">
+        {featuresData.map((item, index)=>(
+          <Feature title={item.title} text={item.text} key={item.title + index} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Features
+export default Features;
